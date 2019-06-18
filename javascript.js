2f9d284aeac0b03d6ac618df0300067af8b68cf9
sigrunlar <48434610+sigrunlar@users.noreply.github.com>
2019-06-18 21:24:03 +0200

    Uploading main files

---
+++ javascript.js
@@ -0,0 +1,57 @@
+function show_hide() {
+    var click = document.getElementById("project1") ;
+    if(click.style.display === "none") {
+        click.style.display = "block" ;
+    }
+    else {
+        click.style.display = "none";
+    }
+
+}
+
+function show_hide1() {
+    var click = document.getElementById("baElement");
+    if(click.style.display === "none") {
+        click.style.display = "block" ;
+    }
+    else {
+        click.style.display = "none";
+    }
+
+}
+
+function show_hide2() {
+    var click = document.getElementById("baContent");
+    if(click.style.display === "none") {
+        click.style.display = "block" ;
+    }
+    else {
+        click.style.display = "none";
+    }
+
+}
+
+function show_hide3() {
+    var click = document.getElementById("baUX");
+    if(click.style.display === "none") {
+        click.style.display = "block" ;
+    }
+    else {
+        click.style.display = "none";
+    }
+
+}
+
+function show_hide4() {
+    var click = document.getElementById("baWeb");
+    if(click.style.display === "none") {
+        click.style.display = "block" ;
+    }
+    else {
+        click.style.display = "none";
+    }
+
+}
+
+
+
